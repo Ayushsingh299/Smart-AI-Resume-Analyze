e2a3c249da3e447a267f8dd42b0c176cb0693a21
Ayush Singh <ayush.24scse1420165@galgotiasuniversity.ac.in>
2026-02-13 10:04:49 +0530

    initial commit

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,19 +2,14 @@
 Utils package for Smart Resume AI
 Centralized imports for cleaner architecture
 """
-
 from .resume_analyzer import ResumeAnalyzer
-from .resume_builder import ResumeBuilder
 from .resume_parser import ResumeParser
 from .excel_manager import ExcelManager
-from .database import DatabaseManager   # replace * with exact class
+from .database import DatabaseManager
 
-
-# Optional 
 __all__ = [
     "ResumeAnalyzer",
-    "ResumeBuilder",
     "ResumeParser",
     "ExcelManager",
-    "DatabaseManager"
+    "DatabaseManager",
 ]
