8d2a1295eb6689b595f5a1e7d932d6f1d2d5a6a9
Ayush Singh <ayush.24scse1420165@galgotiasuniversity.ac.in>
2026-02-14 12:39:27 +0530

    initial commit

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,7 +5,8 @@ Centralized imports for cleaner architecture
 from .resume_analyzer import ResumeAnalyzer
 from .resume_parser import ResumeParser
 from .excel_manager import ExcelManager
-from .database import DatabaseManager
+from .database import Database as DatabaseManager
+
 
 __all__ = [
     "ResumeAnalyzer",
